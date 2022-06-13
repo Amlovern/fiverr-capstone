01f670d0abdd6d118dd630040eb09abe5172e96c
Anthony Lovern <voltorodarkwatch@gmail.com>
2022-06-12 21:27:07 -0400

    Sign up page functionality and styling complete. Need to check on error validation

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,21 +1,22 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
-import { Redirect } from 'react-router-dom';
-import { signUp } from '../../store/session';
+import { Redirect, useHistory } from 'react-router-dom';
+import * as sessionActions from '../../store/session'
 
 const SignUpForm = () => {
   const [errors, setErrors] = useState([]);
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const onSignUp = async (e) => {
     e.preventDefault();
-    if (password === repeatPassword) {
-      const data = await dispatch(signUp(username, email, password));
+    if (password === confirmPassword) {
+      const data = await dispatch(sessionActions.signUp(username, email, password));
       if (data) {
         setErrors(data)
       }
@@ -34,60 +35,110 @@ const SignUpForm = () => {
     setPassword(e.target.value);
   };
 
-  const updateRepeatPassword = (e) => {
-    setRepeatPassword(e.target.value);
+  const updateConfirmPassword = (e) => {
+    setConfirmPassword(e.target.value);
   };
+
+  const demoLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const data = await dispatch(
+        sessionActions.login({
+          credential: 'Voltoro',
+          password: 'Num14Eva'
+        })
+      );
+
+      if (data) setErrors(data)
+    } catch (errorResponse) {
+      console.log(errorResponse)
+    }
+  }
+
+  const loginRedirect = (e) => {
+    e.preventDefault();
+    return history.push('/login')
+  }
 
   if (user) {
     return <Redirect to='/' />;
   }
 
   return (
-    <form onSubmit={onSignUp}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label>User Name</label>
-        <input
-          type='text'
-          name='username'
-          onChange={updateUsername}
-          value={username}
-        ></input>
-      </div>
-      <div>
-        <label>Email</label>
-        <input
-          type='text'
-          name='email'
-          onChange={updateEmail}
-          value={email}
-        ></input>
-      </div>
-      <div>
-        <label>Password</label>
-        <input
-          type='password'
-          name='password'
-          onChange={updatePassword}
-          value={password}
-        ></input>
-      </div>
-      <div>
-        <label>Repeat Password</label>
-        <input
-          type='password'
-          name='repeat_password'
-          onChange={updateRepeatPassword}
-          value={repeatPassword}
-          required={true}
-        ></input>
-      </div>
-      <button type='submit'>Sign Up</button>
-    </form>
+    <div className='sign-up-page'>
+      <form onSubmit={onSignUp} className='sign-up-form'>
+      <section className='form-header'>Join Nerdrr</section>
+        <div>
+          {errors.map((error, ind) => (
+            <div key={ind}>{error}</div>
+            ))}
+        </div>
+        <div className='input-wrapper'>
+          <label>Username</label>
+          <input
+            className='sign-up-input'
+            type='text'
+            name='username'
+            placeholder='Username'
+            onChange={updateUsername}
+            value={username}
+            ></input>
+        </div>
+        <div className='input-wrapper'>
+          <label>Email</label>
+          <input
+            className='sign-up-input'
+            type='text'
+            name='email'
+            placeholder='Email'
+            onChange={updateEmail}
+            value={email}
+            ></input>
+        </div>
+        <div className='input-wrapper'>
+          <label>Password</label>
+          <input
+            className='sign-up-input'
+            type='password'
+            name='password'
+            placeholder='Password'
+            onChange={updatePassword}
+            value={password}
+            ></input>
+        </div>
+        <div className='input-wrapper'>
+          <label>Confirm Password</label>
+          <input
+            className='sign-up-input'
+            type='password'
+            name='confirm_password'
+            placeholder='Confirm Password'
+            onChange={updateConfirmPassword}
+            value={confirmPassword}
+            required={true}
+            ></input>
+        </div>
+        <div className='sign-up-btn-container'>
+          <button className='sign-up-btn' type='submit'>Join</button>
+          <button
+            className='login-btn'
+            type='button'
+            onClick={demoLogin}
+            >
+            Demo Log In
+          </button>
+        </div>
+      </form>
+      <div className='login-footer'>
+          <p>Already a member?</p>
+          <button 
+          className='login-register-redirect'
+          onClick={loginRedirect}
+          >
+          Sign in
+          </button>
+        </div>
+    </div>
   );
 };
 
