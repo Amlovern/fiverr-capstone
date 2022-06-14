c9f08ce4b5b9063dd32bbbfa4107a36cdf175398
Anthony Lovern <voltorodarkwatch@gmail.com>
2022-06-13 20:23:12 -0400

    Forgot to save

--- react-app/src/components/AddOrderForm/index.js
+++ react-app/src/components/AddOrderForm/index.js
@@ -2,14 +2,36 @@ import './AddOrderForm.css';
 
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 
 import * as orderActions from '../../store/order';
 
 export default function AddOrderForm() {
     const dispatch = useDispatch();
     const history = useHistory();
+    const params = useParams();
     const currentUser = useSelector((state) => state.session.user);
+    const gigs = useSelector((state) => state.gig);
+
+    const gigId = params.gigId;
+    const currentGig = gigs.gigsByGigId[gigId];
+    const deliveryTimeline = currentGig.deliveryTimeline;
+
+    const [deliveryInstructions, setDeliveryInstructions] = useState('');
+    const [orderErrors, setOrderErrors] = useState([]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setOrderErrors([])
+
+        const formData = {
+            userId: currentUser.id,
+            gigId: gigId,
+            deliveryInstructions: deliveryInstructions,
+            placed: Date.now(),
+            due
+        }
+    }
 
     return (
         <h1>This is the Add Order Form!</h1>
