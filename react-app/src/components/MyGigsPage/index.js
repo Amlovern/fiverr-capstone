4e27182cd15351e83ce6b50a3c23d7de9c3aab41
Anthony Lovern <voltorodarkwatch@gmail.com>
2022-07-19 16:20:43 -0400

    Finished basic styling for my-gigs page

--- react-app/src/components/MyGigsPage/index.js
+++ react-app/src/components/MyGigsPage/index.js
@@ -1,6 +1,6 @@
 import './MyGigsPage.css';
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import * as gigActions from '../../store/gig';
@@ -11,26 +11,47 @@ export default function MyGigsPage() {
     const sessionUser = useSelector((state) => state.session.user);
     const allGigs = useSelector((state) => state.gig.gigsByOwnerId);
 
-    const usersGigs = allGigs[sessionUser.id];
+    const [usersGigs, setUsersGigs] = useState([])
 
     useEffect(() => {
         dispatch(gigActions.getAllGigsThunk())
     }, [dispatch])
 
+    useEffect(() => {
+        setUsersGigs(allGigs[sessionUser.id]);
+    }, [allGigs, sessionUser.id])
+
     if (!sessionUser) {
         return null;
     };
 
-    return (
-        <div>
-            {usersGigs.length > 0 && (
-                <div>
-                    <div className='my-gigs-header-container'>
-                        <h2 className='my-gigs-header'>My Gigs</h2>
-                        <div className='new-gig-link-container'>
-                            <a href='/gigs/new'>New Gig</a>
-                        </div>
+    if (!usersGigs) {
+        return (
+            <div className='my-gigs-page'>
+                <div className='my-gigs-header-container'>
+                    <h2 className='my-gigs-header'>{sessionUser.username}'s Gigs</h2>
+                </div>
+                <div className='no-gigs-found'>
+                    <h3 className='no-gigs-header'>It seems you don't have any active Gigs</h3>
+                    <p className='no-gigs-subheader'>Click here to create your first Gig:</p>
+                    <div className='new-gig-link-container'>
+                        <a href='/gigs/new'>New Gig</a>
                     </div>
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <div className='my-gigs-page'>
+            <div className='my-gigs-header-container'>
+                <h2 className='my-gigs-header'>{sessionUser.username}'s Gigs</h2>
+                <div className='new-gig-link-container'>
+                    <a href='/gigs/new'>New Gig</a>
+                </div>
+            </div>
+            {usersGigs?.length > 0 && (
+                <div>
                     <div className='search-results-grid'>
                         {usersGigs.map((gig, idx) => (
                             <div className='gig-card-layout' key={idx}>
@@ -60,7 +81,7 @@ export default function MyGigsPage() {
                     ))}
                     </div>
                 </div>
-            )}
+            )}            
         </div>
     )
 }
