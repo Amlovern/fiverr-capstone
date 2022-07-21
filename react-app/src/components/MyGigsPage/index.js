166b97959edd4811f90a78889e7b6788068ec6d6
Anthony Lovern <voltorodarkwatch@gmail.com>
2022-07-21 14:44:49 -0400

    my-gigs images styled

--- react-app/src/components/MyGigsPage/index.js
+++ react-app/src/components/MyGigsPage/index.js
@@ -59,7 +59,7 @@ export default function MyGigsPage() {
                                 <a href={`/gigs/${gig.id}`} target="_blank" rel='noreferrer' className='media'>
                                     <div className='slider'>
                                         <div className='slide-preview'>
-                                            <img src={gig.image} alt='gig' />
+                                            <img className='gig-image' src={gig.image} alt='gig' />
                                         </div>
                                     </div>
                                 </a>
